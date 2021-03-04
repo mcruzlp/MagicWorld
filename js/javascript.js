91331155457552a513d6d8a9af2004e5af27a5fe
Cruz López Pérez <mcruzlp@gmail.com>
2021-03-04 23:58:39 +0100

    update

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
   // Add smooth scrolling to all links
   $("a").on('click', function (event) {
-
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
@@ -15,7 +14,6 @@ $(document).ready(function () {
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 2500, function () {
-
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
